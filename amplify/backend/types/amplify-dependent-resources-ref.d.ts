0996c468ec8fc45c10c8d5ac6fd204cd6883ae70
rajnesah <rajnesah@gmail.com>
2024-05-03 21:07:05 -0700

    Initial Commit with Auth and API

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,29 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "todoApi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "auth": {
+    "vitereactamplified71a91642": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "todoFunction": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
---
+++ src/App.tsx
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+import { post } from 'aws-amplify/api';
+import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { type AuthUser } from "aws-amplify/auth";
+import { type UseAuthenticator } from "@aws-amplify/ui-react-core";
+
+async function postTodo() {
+    try {
+        const restOperation = post({
+            apiName: 'todoApi',
+            path: '/todo',
+            options: {
+                body: {
+                    message: 'Mow the lawn'
+                }
+            }
+        });
+
+        const { body } = await restOperation.response;
+        const response = await body.json();
+
+        console.log('POST call succeeded');
+        console.log(response);
+    } catch (e) {
+        console.log('POST call failed: ', JSON.parse(e.response.body));
+    }
+}
+
+type AppProps = {
+    signOut?: UseAuthenticator["signOut"]; //() => void;
+    user?: AuthUser;
+};
+
+const App: React.FC<AppProps> = ({ signOut, user }) => {
+  const [count, setCount] = useState(0)
+  postTodo()
+    console.log('User', user)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <Heading level={1}>Hello {user.signInDetails.loginId}</Heading>
+      <Button onClick={signOut}>Sign out</Button>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default withAuthenticator(App);
